0facd7913f5613f6f6bc250b15bba507c6547823
Quan <bquan9204@gmail.com>
2025-02-26 02:47:00 +0700

    message

--- src/index.js
+++ src/index.js
@@ -5,22 +5,25 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(morgan('combined'));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
-app.engine('hbs', exphbs.engine({extname:'hbs', defaultLayout:'main'}));
+app.engine('hbs', exphbs.engine({ extname: 'hbs', defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+//route init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
